832154e629ac271cf3ffc5958431c2cfd3da1272
hacktobeer <ramsesdebeer@gmail.com>
2021-02-11 16:14:34 +0100

    Add googleclouddisk, rawdisk and compresseddirectory.

--- turbinia/api-server/turbiniaapi.py
+++ turbinia/api-server/turbiniaapi.py
@@ -7,13 +7,14 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 import uuid
 import os
 from enum import Enum
 import getpass
 
 from turbinia import client as TurbiniaClientProvider
+from turbinia import TurbiniaException
 from turbinia.client import TurbiniaCeleryClient
 from turbinia import config
 from turbinia import evidence
@@ -21,15 +22,40 @@ from turbinia.processors import archive
 from turbinia.message import TurbiniaRequest
 
 
-class Command(str, Enum):
-  alexnet = "compresseddirectory"
-  resnet = "rawdisk"
-  lenet = "googleclouddisk"
+class Generic(BaseModel):
+  job_allowlist: Optional[List[str]]
+  job_denylist: Optional[List[str]]
+  source_path: str
+  output_json: Optional[bool]
+
+
+class GCP(BaseModel):
+  disk_name: Optional[str]
+  project_name: Optional[str]
+  zone: Optional[str]
+
+
+class RequestOptions(BaseModel):
+  generic: Optional[Generic]
+  GCP: Optional[GCP]
 
 
 class Request(BaseModel):
-  source_path: str
   request_id: Optional[str]
+  request_options: Optional[RequestOptions]
+
+
+class Evidence(str, Enum):
+  compresseddirectory = "compresseddirectory"
+  rawdisk = "rawdisk"
+  googleclouddisk = "googleclouddisk"
+  config = "config"
+  apfs = "apfs"
+  bitlocker = "bitlocker"
+  googleclouddiskembedded = "googleclouddiskembedded"
+  rawmemory = "rawmemory"
+  directory = "directory"
+  gcplogs = "gcplogs"
 
 
 client = TurbiniaClientProvider.get_turbinia_client(False)
@@ -53,31 +79,32 @@ def statusrequests():
 
 
 @api.get("/status/request/{request_id}")
-def statusrequest(request_id: uuid.UUID):
-  # return client.format_task_status(
-  #         instance=config.INSTANCE_ID, project=config.TURBINIA_PROJECT,
-  #         region=region, days=7, task_id="",
-  #         request_id=request_id, user="",
-  #         all_fields=False, full_report=False,
-  #         priority_filter=20, output_json=False)
+def statusrequest(request_id: str):
   return client.format_task_status(
       instance=config.INSTANCE_ID, project=config.TURBINIA_PROJECT,
-      region=region, days=1, task_id=None, request_id=request_id, user=None,
-      all_fields=False, full_report=False, priority_filter=20, output_json=True)
-  # return client.format_task_statistics(
-  #           instance=config.INSTANCE_ID, project=config.TURBINIA_PROJECT,
-  #           region=region, days=1, task_id=None,
-  #           request_id=request_id, user=None, csv=False)
+      region=region, request_id=request_id, full_report=True, output_json=True)
 
 
-@api.post("/create/request/{evidence_type}")
-def createrequest(evidence_type: Command, request: Request):
+@api.post("/create/{evidence_type}")
+def createrequest(evidence_type: Evidence, req: Request):
   request_id = uuid.uuid4().hex
 
-  #archive.ValidateTarFile(source_path)
-  source_path = os.path.abspath(request.source_path)
-  evidence_ = evidence.CompressedDirectory(
-      name=evidence_type, source_path=source_path, source=None)
+  if evidence_type == Evidence.compresseddirectory:
+    source_path = os.path.abspath(req.request_options.generic.source_path)
+    evidence_ = evidence.CompressedDirectory(source_path=source_path)
+  if evidence_type == Evidence.rawdisk:
+    source_path = os.path.abspath(req.request_options.generic.source_path)
+    evidence_ = evidence.RawDisk(
+        name=evidence_type, source_path=source_path, source=None)
+  if evidence_type == Evidence.googleclouddisk:
+    evidence_ = evidence.GoogleCloudDisk(
+        project=req.options.GCP.zone, zone=req.options.GCP.zone,
+        disk_name=req.options.GCP.disk_name)
+
+  try:
+    evidence_.validate()
+  except TurbiniaException as exception:
+    print(exception)
 
   request = TurbiniaRequest(request_id=request_id, requester=getpass.getuser())
   request.evidence.append(evidence_)
